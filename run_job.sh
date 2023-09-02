3e0eeb6a6b891483cff7c95bb4dfd52ee8a8562a
joon <joonable2@gmail.com>
2023-09-02 18:37:21 +0100

    initial commit

--- run_job.sh
+++ run_job.sh
@@ -3,15 +3,15 @@ list_job_cds=()
 
 for _job_cd in "${list_job_cds[@]}"; do
   if [[ "$_job_cd" == "clm"* ]]; then
-    python ./utils/run_clm.py --job_cd _job_cd
+    python ./utils/run_clm.py --job_cd $_job_cd
   elif [[ "$_job_cd" == "loc"* ]]; then
-    python ./utils/run_loc.py --job_cd _job_cd
+    python ./utils/run_loc.py --job_cd $_job_cd
   elif [[ "$_job_cd" == "pft"* ]]; then
-    python ./utils/run_pft.py --job_cd _job_cd
+    python ./utils/run_pft.py --job_cd $_job_cd
   elif [[ "$_job_cd" == "prn"* ]]; then
-    python ./utils/run_prn.py --job_cd _job_cd
+    python ./utils/run_prn.py --job_cd $_job_cd
   elif [[ "$_job_cd" == "eval"* ]]; then
-    python ./utils/run_eval.py --job_cd _job_cd
+    python ./utils/run_eval.py --job_cd $_job_cd
   else
     echo "Wrong"
   fi
